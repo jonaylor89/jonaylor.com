e304d0c72b3da8fb74830df8d9fd8a99fe236fc6
Johannes Naylor <jonaylor89@gmail.com>
2025-11-04 13:47:50 -0500

    add scriptName to plausible

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { withPlausibleProxy } from "next-plausible";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPlausibleProxy({
+  scriptName: "pa-GVZs6Q7bbqpA-DUmrYk1p",
+})(nextConfig);
