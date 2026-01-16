3aeb0dd57e3fbf12456e939897836d459a65c0c9
Johannes Naylor <jonaylor89@gmail.com>
2026-01-16 12:42:39 -0300

    delete old projects

--- apps/linktree/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import { withPlausibleProxy } from "next-plausible";
-
-const nextConfig: NextConfig = {
-	/* config options here */
-};
-
-export default withPlausibleProxy()(nextConfig);
