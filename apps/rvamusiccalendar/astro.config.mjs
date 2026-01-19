8cc86d79783ca82d9c4b8c6922a71c9dd14f65cc
Johannes Naylor <jonaylor89@gmail.com>
2026-01-19 11:59:44 -0500

    Vercel analytics => plausible

--- apps/rvamusiccalendar/astro.config.mjs
+++ apps/rvamusiccalendar/astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
 });
