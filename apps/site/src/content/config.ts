be7942eda22ea2917a26f8e8429507b4a86c32ef
Johannes Naylor <jonaylor89@gmail.com>
2026-01-30 12:34:29 -0500

    add lang attr to posts

--- apps/site/src/content/config.ts
+++ apps/site/src/content/config.ts
@@ -11,6 +11,7 @@ const posts = defineCollection({
 			coverImage: image().optional(),
 			coverImageAlt: z.string().optional(),
 			draft: z.boolean().default(false),
+			lang: z.enum(["en", "de"]).default("en"),
 		}),
 });
 
